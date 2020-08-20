a8167b6d15daa66ab0d7e5583e7e63675cd89dd3
Tai Nguyen <tainc@ftcjsc.com>
2020-08-20 14:09:37 +0700

    add lazy load image

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -281,6 +281,37 @@
 <script type="text/javascript" src="/js/custom.js"></script>
 
 <script type="text/javascript">
+    document.addEventListener("DOMContentLoaded", function() {
+      var lazyloadImages = document.querySelectorAll("img.lazy");    
+      var lazyloadThrottleTimeout;
+      
+      function lazyload () {
+        if(lazyloadThrottleTimeout) {
+          clearTimeout(lazyloadThrottleTimeout);
+        }    
+        
+        lazyloadThrottleTimeout = setTimeout(function() {
+            var scrollTop = window.pageYOffset;
+            lazyloadImages.forEach(function(img) {
+                if(img.offsetTop < (window.innerHeight + scrollTop)) {
+                  img.src = img.dataset.src;
+                  img.classList.remove('lazy');
+                }
+            });
+            if(lazyloadImages.length == 0) { 
+              document.removeEventListener("scroll", lazyload);
+              window.removeEventListener("resize", lazyload);
+              window.removeEventListener("orientationChange", lazyload);
+            }
+        }, 20);
+      }
+      
+      document.addEventListener("scroll", lazyload);
+      window.addEventListener("resize", lazyload);
+      window.addEventListener("orientationChange", lazyload);
+    });
+
+    
     var latitude = 34.038405;
     var longitude = -117.946944;
     var markerImage = "/images/map-marker.png";
